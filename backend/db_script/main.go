53af02bab01e64f1a2cae60ea24bfd92e6507160
Roninnv <AsA828A@outlook.com>
2024-11-23 19:46:55 +0800

    Sat Nov 23 07:46:55 PM CST 2024

--- backend/db_script/main.go
+++ backend/db_script/main.go
@@ -16,15 +16,27 @@ var db *gorm.DB
 var err error
 var logFile *os.File
 
-func init() {
-	logFile, err = os.OpenFile("./log/init.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+func openLogFile() {
+	if err := os.MkdirAll("./log", 0755); err != nil {
+		log.Fatalf("Failed to create log directory: %v", err)
+	}
+
+	logFile, err = os.OpenFile("./log/run.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Println("Failed to create or open log file:", err)
 		panic("Unable to initialize log file")
 	}
 	log.SetOutput(logFile)
+}
 
-	var dsn string = "root:@Faj872@tcp(127.0.0.1:3306)/?charset=utf8mb4&parseTime=True&loc=Local"
+func connectDatabase(databaseConfig lib.Config) {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/?charset=utf8mb4&parseTime=True&loc=Local",
+		databaseConfig.Database.Username,
+		databaseConfig.Database.Password,
+		databaseConfig.Database.Host,
+		databaseConfig.Database.Port,
+	)
 
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -32,6 +44,7 @@ func init() {
 		panic("Unable to connect to database")
 	}
 	log.Println("Database connection successful")
+	fmt.Println("Database connection successful")
 }
 
 func executeSQLFromFile(filename string) error {
@@ -41,7 +54,6 @@ func executeSQLFromFile(filename string) error {
 	}
 
 	sqlStatements := strings.Split(string(content), ";")
-
 	for _, stmt := range sqlStatements {
 		stmt = strings.TrimSpace(stmt)
 		if stmt != "" {
@@ -57,26 +69,29 @@ func executeSQLFromFile(filename string) error {
 }
 
 func main() {
-	/*
-		defer logFile.Close()
+	openLogFile()
+	defer logFile.Close()
 
-		err := executeSQLFromFile("../db/migrations/202411161001_init.sql")
-		if err != nil {
-			log.Fatalf("failed to initialize the database: %v", err)
-		}
-
-		log.Println("Database initialization succeeded")
-		fmt.Println("Database initialization succeeded")
-	*/
-	config, err := lib.LoadConfig("./database_config.yaml")
+	databaseConfig, err := lib.LoadConfig("./database_config.yaml")
 	if err != nil {
 		log.Fatalf("Error loading config: %v", err)
 	}
 
-	// 输出解析后的配置
-	fmt.Printf("Database Config:\n")
-	fmt.Printf("Host: %s\n", config.Database.Host)
-	fmt.Printf("Port: %d\n", config.Database.Port)
-	fmt.Printf("Username: %s\n", config.Database.Username)
-	fmt.Printf("Password: %s\n", config.Database.Password)
+	connectDatabase(databaseConfig)
+
+	err = executeSQLFromFile("./db/migrations/warehouse_init.sql")
+	if err != nil {
+		log.Fatalf("failed to initialize the database: %v", err)
+	}
+
+	log.Println("Database initialization succeeded")
+	fmt.Println("Database initialization succeeded")
+
+	err = executeSQLFromFile("./db/seed/warehouse_seed.sql")
+	if err != nil {
+		log.Fatalf("failed to insert data into the database: %v", err)
+	}
+
+	log.Println("Data initialization succeeded")
+	fmt.Println("Data initialization succeeded")
 }
